8a909e6d63ebd32fcfba7bf7cf54f42cf13187c7
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-09-29 15:05:31 -0300

    refact(model): Rename package ServicoEstetico

--- src/main/java/com/br/alarconvinicius/sistemadecontroledeprocedimento/Database/Models/ServicoEstetico.java
+++ src/main/java/com/br/alarconvinicius/sistemadecontroledeprocedimento/Database/Models/ServicoEstetico.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.br.alarconvinicius.sistemadecontroledeprocedimento.Models;
+package com.br.alarconvinicius.sistemadecontroledeprocedimento.Database.Models;
 
 /**
  *
  * @author Alarcon Vinicius
  */
 public class ServicoEstetico {
+
     private String nome;
     private String descricao;
     private double preco;
@@ -43,6 +43,5 @@ public class ServicoEstetico {
     public void setPreco(double preco) {
         this.preco = preco;
     }
-    
-    
+
 }
