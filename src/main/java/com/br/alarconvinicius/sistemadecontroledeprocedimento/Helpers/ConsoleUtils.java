c02ba53fed6530fc18b1be060097c4e6d28e27a3
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-09-29 22:02:05 -0300

    feat(helper): Add ConsoleUtils

---
+++ src/main/java/com/br/alarconvinicius/sistemadecontroledeprocedimento/Helpers/ConsoleUtils.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.br.alarconvinicius.sistemadecontroledeprocedimento.Helpers;
+
+/**
+ *
+ * @author Alarcon Vinicius
+ */
+public class ConsoleUtils {
+
+    // Método para limpar o terminal no Windows
+    public static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                for (int i = 0; i < 50; i++) {
+                    System.out.println();
+                }
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (final Exception e) {
+        }
+    }
+}
+
