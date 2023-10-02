42fd18c3936d8d28071a047299bfa6f5c40c1034
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-10-02 06:53:32 -0300

    refact(service): Refact services

--- src/Business/Services/MainSystemService.java
+++ src/Business/Services/MainSystemService.java
@@ -2,30 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.br.alarconvinicius.sistemadecontroledeprocedimento.Business;
 
-import com.br.alarconvinicius.sistemadecontroledeprocedimento.Database.Models.RegistroProcedimento;
-import com.br.alarconvinicius.sistemadecontroledeprocedimento.Database.Models.ServicoEstetico;
-import com.br.alarconvinicius.sistemadecontroledeprocedimento.Database.Repositories.RegistroProcedimentoRepository;
-import com.br.alarconvinicius.sistemadecontroledeprocedimento.Database.Repositories.ServicoEsteticoRepository;
+package Business.Services;
+
+import Database.Models.AestheticProcedures;
+import Database.Models.ProceduresPerformed;
+import Database.Repositories.AestheticProceduresRepository;
+import Database.Repositories.ProceduresPerformedRepository;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  *
  * @author Alarcon Vinicius
  */
-public class SistemaControleProcedimentoService {
+public class MainSystemService {
 
-    private final ServicoEsteticoRepository _servicoRepository;
-    private final RegistroProcedimentoRepository _registroRepository;
-
-    public SistemaControleProcedimentoService(ServicoEsteticoRepository servicoRepository, RegistroProcedimentoRepository registroRepository) {
-        _servicoRepository = servicoRepository;
-        _registroRepository = registroRepository;
+    private final AestheticProceduresRepository _aProcedureRepository;
+    private final ProceduresPerformedRepository _pPerformedRepository;
+    public MainSystemService(AestheticProceduresRepository aProcedureRepository, ProceduresPerformedRepository pPerformedRepository) {
+        _aProcedureRepository = aProcedureRepository;
+        _pPerformedRepository = pPerformedRepository;
     }
 
     // Método para retornar menu de cadastro de serviços disponíveis
-    public void cadastrarServicos(Scanner scanner) {
+    public void createAestheticProcedure(Scanner scanner) {
         System.out.println("");
         System.out.println("Cadastrar Serviço: ");
         System.out.println("");
@@ -45,11 +46,11 @@ public class SistemaControleProcedimentoService {
 
         System.out.println("");
 
-        _servicoRepository.cadastrarServico(new ServicoEstetico(nome, descricao, valor));
+        _aProcedureRepository.create(new AestheticProcedures(nome, descricao, valor));
     }
 
     // Método para retornar menu de cadastro de serviços disponíveis
-    public void cadastrarProcedimentoRealizado(Scanner scanner) {
+    public void createProcedurePerformed(Scanner scanner) {
         System.out.println("");
         System.out.println("Cadastrar Procedimento Realizado: ");
         System.out.println("");
@@ -61,16 +62,11 @@ public class SistemaControleProcedimentoService {
 
         System.out.println("");
 
-        if (escolhaServico >= 1 && escolhaServico <= _servicoRepository.listarServicos().size()) {
-            ServicoEstetico servicoEscolhido = _servicoRepository.listarServicos().get(escolhaServico - 1);
+        if (escolhaServico >= 1 && escolhaServico <= _aProcedureRepository.findAll().size()) {
+            AestheticProcedures servicoEscolhido = _aProcedureRepository.findAll().get(escolhaServico - 1);
 
             System.out.print("Data: ");
             String data = scanner.nextLine();
-
-            System.out.println("");
-
-            System.out.print("Descrição: ");
-            String descricao = scanner.nextLine();
 
             System.out.println("");
 
@@ -79,7 +75,7 @@ public class SistemaControleProcedimentoService {
 
             System.out.println("");
 
-            _registroRepository.cadastrarRegistro(new RegistroProcedimento(data, servicoEscolhido, valor));
+            _pPerformedRepository.create(new ProceduresPerformed(data, servicoEscolhido.getId(), valor));
         } else {
             System.out.println("Opção inválida. Escolha um número de serviço válido.");
         }
@@ -87,30 +83,29 @@ public class SistemaControleProcedimentoService {
 
     // Método para retornar menu de serviços disponíveis
     public void menuServicos() {
-        var servicosDb = _servicoRepository.listarServicos();
+         List<AestheticProcedures> servicosDb = _aProcedureRepository.findAll();
 
         System.out.println("");
         System.out.println("Serviços disponíveis: ");
         System.out.println("");
-        for (int i = 0; i < servicosDb.size(); i++) {
-            ServicoEstetico servico = servicosDb.get(i);
-            System.out.println("Serviço (" + (i + 1) + "): " + servico.getNome());
-            System.out.println("-------------------------");
-        }
+        for (AestheticProcedures servico : servicosDb) {
+        System.out.println("Serviço (" + servico.getId() + "): " + servico.getName());
+        System.out.println("-------------------------");
+    }
         System.out.println("");
     }
 
     // Método para gerar um relatório de procedimentos
     public void gerarRelatorioProcedimentos() {
-        var registrosDb = _registroRepository.listarRegistros();
+        List<ProceduresPerformed> registrosDb = _pPerformedRepository.findAll();
         double receitaMensal = calcularReceitaMensal();
         System.out.println("");
         System.out.println("Receita Mensal: R$" + receitaMensal);
         System.out.println("");
-        for (RegistroProcedimento registro : registrosDb) {
-            System.out.println("Data: " + registro.getData());
-            System.out.println("Serviço: " + registro.getServico().getNome());
-            System.out.println("Valor Recebido: " + registro.getValorRecebido());
+        for (ProceduresPerformed registro : registrosDb) {
+            System.out.println("Data: " + registro.getDate());
+            System.out.println("Serviço: " + registro.getProcedure().getName());
+            System.out.println("Valor Recebido: " + registro.getAmountReceived());
             System.out.println("-------------------------");
         }
         System.out.println("");
@@ -118,10 +113,10 @@ public class SistemaControleProcedimentoService {
 
     // Método para calcular a receita mensal da clínica
     private double calcularReceitaMensal() {
-        var registrosDb = _registroRepository.listarRegistros();
+        var registrosDb = _pPerformedRepository.findAll();
         double receita = 0.0;
-        for (RegistroProcedimento registro : registrosDb) {
-            receita += registro.getValorRecebido();
+        for (ProceduresPerformed registro : registrosDb) {
+            receita += registro.getAmountReceived();
         }
         return receita;
     }
