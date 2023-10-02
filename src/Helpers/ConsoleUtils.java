d07acf0896a0f3e87c5c9143b3239aed59379758
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-10-02 07:05:59 -0300

    refact(helper): Refact ConsoleUtils

--- src/Helpers/ConsoleUtils.java
+++ src/Helpers/ConsoleUtils.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package com.br.alarconvinicius.sistemadecontroledeprocedimento.Helpers;
+package Helpers;
 
 /**
  *
@@ -25,4 +25,3 @@ public class ConsoleUtils {
         }
     }
 }
-
