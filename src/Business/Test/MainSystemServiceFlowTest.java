e1eda878a4145938228c9a6b34235deb372f1c10
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-10-02 19:05:12 -0300

    feat(test): Add menu options

--- src/Business/Test/MainSystemServiceFlowTest.java
+++ src/Business/Test/MainSystemServiceFlowTest.java
@@ -10,6 +10,7 @@ import Business.Services.MainSystemService;
 import Database.Models.User;
 import Database.Repositories.AestheticProceduresRepository;
 import Database.Repositories.ProceduresPerformedRepository;
+import Database.Repositories.UserRepository;
 import Helpers.ConsoleUtils;
 import java.util.Scanner;
 
@@ -20,16 +21,27 @@ import java.util.Scanner;
 public class MainSystemServiceFlowTest {
 public static void main(String[] args) {
         User auth = new User();
+        UserRepository userRepository = new UserRepository();
         AestheticProceduresRepository aesProduceresRepository = new AestheticProceduresRepository();
         ProceduresPerformedRepository procePerformedRepository = new ProceduresPerformedRepository();
-        MainSystemService sistema = new MainSystemService(aesProduceresRepository, procePerformedRepository);
+        MainSystemService sistema = new MainSystemService(aesProduceresRepository, procePerformedRepository, userRepository);
         UserService userService = new UserService(auth);
 
         
         Scanner scanner = new Scanner(System.in);
         
-        var logado = userService.authenticate(scanner);
+        System.out.println("");
+        System.out.println("***********************");
+        System.out.println("* Sistema de Controle *");
+        System.out.println("*   de Procedimento   *");
+        System.out.println("***********************");
+        System.out.println("");
+        System.out.println("");
 
+        System.out.println("* Faça o Login para acessar o sistema: *");
+        System.out.println("");
+        var logado = userService.authenticate(scanner);
+//        var logado = true;
         while (logado) {
             System.out.println("");
             System.out.println("Menu");
@@ -37,7 +49,10 @@ public static void main(String[] args) {
             System.out.println("1 - Cadastrar Serviços");
             System.out.println("2 - Cadastrar Procedimento");
             System.out.println("3 - Relatório de Procedimentos");
-            System.out.println("4 - Sair");
+            System.out.println("4 - Listar Serviços");
+            System.out.println("5 - Visualizar perfil");
+            System.out.println("6 - Atualizar perfil");
+            System.out.println("7 - Sair");
             System.out.print("Escolha uma opção: ");
             String opcaoMenu = scanner.nextLine();
             switch (Integer.parseInt(opcaoMenu)) {
@@ -60,6 +75,24 @@ public static void main(String[] args) {
                     System.out.println("");
                     break;
                 case 4:
+                    ConsoleUtils.clearConsole();
+                    System.out.println("");
+                    sistema.getAllAestheticProcedures();
+                    System.out.println("");
+                    break;
+                case 5:
+                    ConsoleUtils.clearConsole();
+                    System.out.println("");
+                    sistema.getProfile();
+                    System.out.println("");
+                    break;
+                case 6:
+                    ConsoleUtils.clearConsole();
+                    System.out.println("");
+                    sistema.updateProfile(scanner);
+                    System.out.println("");
+                    break;
+                case 7:
                     ConsoleUtils.clearConsole();
                     System.out.println("");
                     System.out.println("Encerrando o programa.");
